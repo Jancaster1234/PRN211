db3cc4b6bbf34b491745b849c1fcafe8634df0d8
kisphapha <thanhhung16082003@gmail.com>
2023-11-04 22:23:43 +0700

    Save changes

--- MeetMyLecturerWinApp/frmMain.Designer.cs
+++ MeetMyLecturerWinApp/frmMain.Designer.cs
@@ -30,20 +30,20 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             panel1 = new Panel();
-            panel2 = new Panel();
-            panel3 = new Panel();
-            pictureBox1 = new PictureBox();
-            lbNavigation = new Label();
-            btnProfile = new Button();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
             button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            btnProfile = new Button();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
             button6 = new Button();
+            lbNavigation = new Label();
+            panel3 = new Panel();
             panel1.SuspendLayout();
-            panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
@@ -62,99 +62,18 @@
             panel1.Size = new Size(145, 450);
             panel1.TabIndex = 0;
             // 
-            // panel2
+            // button5
             // 
-            panel2.BackColor = Color.SlateBlue;
-            panel2.Controls.Add(button6);
-            panel2.Controls.Add(lbNavigation);
-            panel2.Dock = DockStyle.Top;
-            panel2.Location = new Point(145, 0);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(655, 67);
-            panel2.TabIndex = 1;
-            // 
-            // panel3
-            // 
-            panel3.BackColor = Color.DarkSlateBlue;
-            panel3.Dock = DockStyle.Fill;
-            panel3.Location = new Point(145, 67);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(655, 383);
-            panel3.TabIndex = 2;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Dock = DockStyle.Top;
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.InitialImage = null;
-            pictureBox1.Location = new Point(0, 0);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(145, 96);
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
-            // 
-            // lbNavigation
-            // 
-            lbNavigation.AutoSize = true;
-            lbNavigation.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
-            lbNavigation.Location = new Point(22, 9);
-            lbNavigation.Name = "lbNavigation";
-            lbNavigation.Size = new Size(100, 40);
-            lbNavigation.TabIndex = 0;
-            lbNavigation.Text = "Home";
-            // 
-            // btnProfile
-            // 
-            btnProfile.BackColor = Color.SkyBlue;
-            btnProfile.Dock = DockStyle.Top;
-            btnProfile.FlatAppearance.BorderSize = 0;
-            btnProfile.FlatStyle = FlatStyle.Flat;
-            btnProfile.Location = new Point(0, 96);
-            btnProfile.Name = "btnProfile";
-            btnProfile.Size = new Size(145, 39);
-            btnProfile.TabIndex = 1;
-            btnProfile.Text = "Profile";
-            btnProfile.UseVisualStyleBackColor = false;
-            // 
-            // button1
-            // 
-            button1.BackColor = Color.LightSkyBlue;
-            button1.Dock = DockStyle.Top;
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Location = new Point(0, 135);
-            button1.Name = "button1";
-            button1.Size = new Size(145, 39);
-            button1.TabIndex = 2;
-            button1.Text = "Search Teacher";
-            button1.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            button2.BackColor = Color.DeepSkyBlue;
-            button2.Dock = DockStyle.Top;
-            button2.FlatAppearance.BorderSize = 0;
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Location = new Point(0, 174);
-            button2.Name = "button2";
-            button2.Size = new Size(145, 39);
-            button2.TabIndex = 3;
-            button2.Text = "Search Slot";
-            button2.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            button3.BackColor = Color.DodgerBlue;
-            button3.Dock = DockStyle.Top;
-            button3.FlatAppearance.BorderSize = 0;
-            button3.FlatStyle = FlatStyle.Flat;
-            button3.Location = new Point(0, 213);
-            button3.Name = "button3";
-            button3.Size = new Size(145, 39);
-            button3.TabIndex = 4;
-            button3.Text = "Passcode request";
-            button3.UseVisualStyleBackColor = false;
+            button5.BackColor = Color.SlateBlue;
+            button5.Dock = DockStyle.Top;
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Location = new Point(0, 291);
+            button5.Name = "button5";
+            button5.Size = new Size(145, 39);
+            button5.TabIndex = 6;
+            button5.Text = "Action record";
+            button5.UseVisualStyleBackColor = false;
             // 
             // button4
             // 
@@ -169,18 +88,80 @@
             button4.Text = "Meeting request";
             button4.UseVisualStyleBackColor = false;
             // 
-            // button5
+            // button3
             // 
-            button5.BackColor = Color.SlateBlue;
-            button5.Dock = DockStyle.Top;
-            button5.FlatAppearance.BorderSize = 0;
-            button5.FlatStyle = FlatStyle.Flat;
-            button5.Location = new Point(0, 291);
-            button5.Name = "button5";
-            button5.Size = new Size(145, 39);
-            button5.TabIndex = 6;
-            button5.Text = "Action record";
-            button5.UseVisualStyleBackColor = false;
+            button3.BackColor = Color.DodgerBlue;
+            button3.Dock = DockStyle.Top;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(0, 213);
+            button3.Name = "button3";
+            button3.Size = new Size(145, 39);
+            button3.TabIndex = 4;
+            button3.Text = "Passcode request";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DeepSkyBlue;
+            button2.Dock = DockStyle.Top;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(0, 174);
+            button2.Name = "button2";
+            button2.Size = new Size(145, 39);
+            button2.TabIndex = 3;
+            button2.Text = "Search Slot";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightSkyBlue;
+            button1.Dock = DockStyle.Top;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(0, 135);
+            button1.Name = "button1";
+            button1.Size = new Size(145, 39);
+            button1.TabIndex = 2;
+            button1.Text = "Search Teacher";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // btnProfile
+            // 
+            btnProfile.BackColor = Color.SkyBlue;
+            btnProfile.Dock = DockStyle.Top;
+            btnProfile.FlatAppearance.BorderSize = 0;
+            btnProfile.FlatStyle = FlatStyle.Flat;
+            btnProfile.Location = new Point(0, 96);
+            btnProfile.Name = "btnProfile";
+            btnProfile.Size = new Size(145, 39);
+            btnProfile.TabIndex = 1;
+            btnProfile.Text = "Profile";
+            btnProfile.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Top;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.InitialImage = null;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(145, 96);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.SlateBlue;
+            panel2.Controls.Add(button6);
+            panel2.Controls.Add(lbNavigation);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(145, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(655, 67);
+            panel2.TabIndex = 1;
             // 
             // button6
             // 
@@ -195,7 +176,26 @@
             button6.Text = "Logout/ Profile";
             button6.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // lbNavigation
+            // 
+            lbNavigation.AutoSize = true;
+            lbNavigation.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lbNavigation.Location = new Point(22, 9);
+            lbNavigation.Name = "lbNavigation";
+            lbNavigation.Size = new Size(100, 40);
+            lbNavigation.TabIndex = 0;
+            lbNavigation.Text = "Home";
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.DarkSlateBlue;
+            panel3.Dock = DockStyle.Fill;
+            panel3.Location = new Point(145, 67);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(655, 383);
+            panel3.TabIndex = 2;
+            // 
+            // frmMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
@@ -204,12 +204,13 @@
             Controls.Add(panel3);
             Controls.Add(panel2);
             Controls.Add(panel1);
-            Name = "Form1";
+            Name = "frmMain";
             Text = "Form1";
+            Load += frmMain_Load;
             panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
